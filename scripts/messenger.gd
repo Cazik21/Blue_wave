9ff35a283d5688b595e0a4622cb69e830ae726cc
kristofer4521 <christopherjomolca014@gmail.com>
2025-11-18 19:12:20 -0300

    commit 1 do Chris

--- scripts/messenger.gd
+++ scripts/messenger.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var shoot_direction : Vector2
+var direction_for_the_shoot : float
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
 var speed = 100
+var shoot = preload("res://scenes/shoot.tscn")
 
 func _physics_process(_delta: float) -> void:
+	Messenger.direction_for_the_shoot = int(anim.flip_h) - int(int(anim.flip_h) == 0)
 	move()
-	shoot()
+	shooting()
 
 func move() -> void:
 	var direction_vector = Vector2(
@@ -15,9 +19,19 @@ func move() -> void:
 	velocity = direction_vector * speed
 	move_and_slide()
 
-func shoot() -> void:
+func shooting() -> void:
 	Messenger.shoot_direction = Vector2(
 		Input.get_action_strength("shoot_right") - Input.get_action_strength("shoot_left"),
 		Input.get_action_strength("shoot_down") - Input.get_action_strength("shoot_up")
 		)
 	print(Messenger.shoot_direction)
+	if Messenger.shoot_direction > Vector2(0.0, 0.0):
+		var shoot_intantiate = shoot.instantiate()
+		add_child(shoot_intantiate)
+		print(Messenger.direction_for_the_shoot)
+	
+	elif Messenger.shoot_direction < Vector2(0.0, 0.0):
+		var shoot_intantiate = shoot.instantiate()
+		add_sibling(shoot_intantiate)
+		shoot_intantiate.global_position = global_position
+		print(Messenger.direction_for_the_shoot)
