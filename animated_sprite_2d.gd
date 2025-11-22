322176114c161f9440201569657ca6f5be89ebb2
kristofer4521 <christopherjomolca014@gmail.com>
2025-11-22 14:14:37 -0300

    Commit 7

--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -5,25 +5,13 @@ extends AnimatedSprite2D
 @export var enemy_scene : PackedScene
 
 func _ready() -> void:
-	floor_enemy()
+
 	play("1")
 	var tweenop = get_tree().create_tween()
 	tweenop.tween_property(self, "modulate",
 	 Color("#ffffff", 0),
 	 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	
 	tweenop.connect("finished", delete)
 
-func floor_enemy():
-	if Messenger.wave >= 3:
-		spawn_timer.start()
-
 func delete():
-	floor_enemy()
 	queue_free()
-
-func _on_spawn_floor_enemy_timeout() -> void:
-	var enemy = enemy_scene.intantiate()
-	add_sibling(enemy)
-	enemy.global_position = global_position
-	floor_enemy()
