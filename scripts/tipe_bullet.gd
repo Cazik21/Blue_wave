3cbc924d46e75a3dd924139c92335fa2b05d8be8
kristofer4521 <christopherjomolca014@gmail.com>
2025-12-01 14:13:51 -0300

    👽 👽 👽 👽 👽 👽 👽 👽 👽

--- scripts/tipe_bullet.gd
+++ scripts/tipe_bullet.gd
@@ -5,37 +5,41 @@ var scroll_pos = 0
 
 
 func _ready() -> void:
-	scroll_pos = 1
+	Messenger.scroll_pos = 1
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("scoll_up"):
-		scroll_pos = scroll_pos%5 + 1
+		if Messenger.scroll_pos < 5:
+				scroll_pos = scroll_pos%5 + 1
+		else: 
+			Messenger.scroll_pos = 0
 	
 	if Input.is_action_just_pressed("scroll_down"):
-		if scroll_pos > 1:
-			scroll_pos -= 1
-		else:
-			scroll_pos = 5
+		if Messenger.scroll_pos > 0:
+				Messenger.scroll_pos -= 1
+		else: 
+			Messenger.scroll_pos = 4
 	
-	if  scroll_pos == 1:
+	if  Messenger.scroll_pos == 0:
 		Messenger.type_of_the_bullet = "normal"
 	
-	if scroll_pos == 2:
+	if Messenger.scroll_pos == 1:
 		Messenger.type_of_the_bullet = "big"
 	
-	if scroll_pos == 3:
+	if Messenger.scroll_pos == 2:
 		Messenger.type_of_the_bullet = "small"
 	
-	if scroll_pos == 4:
+	if Messenger.scroll_pos == 3:
 		Messenger.type_of_the_bullet = "bomb"
 	
-	if scroll_pos == 5:
+	if Messenger.scroll_pos == 4:
 		Messenger.type_of_the_bullet =  "shock"
 	
 	play(Messenger.type_of_the_bullet)
 	count.global_position.y = 41
-	if Messenger.balas_q_tenho[scroll_pos - 1] < 10:
+	if Messenger.balas_q_tenho[Messenger.scroll_pos -1] < 10:
 		count.position.x = 15
 	else:
 		count.position.x = 6
-	count.text = str(Messenger.balas_q_tenho[scroll_pos - 1])
+	var balinhas = Messenger.balas_q_tenho[Messenger.scroll_pos -1] 
+	count.text = str(balinhas)
