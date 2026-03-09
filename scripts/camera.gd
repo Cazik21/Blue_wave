1a4daa0ca28bd9b8b4489f9cc53787b22b16fbab
kristofer4521 <christopherjomolca014@gmail.com>
2026-03-08 21:15:33 -0300

    Update camera.gd

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,23 +4,13 @@ var tween_in
 var tween_out
 var shakestrenght = 0.0
 
-const INITIALSTRENGHT = 15.0
-const SHAKEFADE = 9.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	shake()
-
-func shake():
-	shakestrenght = INITIALSTRENGHT
-
-func randomOffset() -> Vector2:
-	return Vector2(randf_range(-shakestrenght, shakestrenght),
-	 randf_range(shakestrenght, -shakestrenght))
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	shake()
+func _process(_delta: float) -> void:
 	if Input.is_action_pressed("charge"):
 		tween_in = get_tree().create_tween()
 		tween_in.tween_property(self, "zoom",
@@ -32,4 +22,3 @@ func _process(delta: float) -> void:
 		 Vector2(1.0, 1.0),
 		 0.7)
 	
-	shakestrenght = lerp(shakestrenght, 0.0, SHAKEFADE * delta)
